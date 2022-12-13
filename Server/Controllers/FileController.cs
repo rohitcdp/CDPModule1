cd49a9bd5a5e8b6e504b5190a774856b505659d4
rohit.pawar <rohit.pawar@hiteshi.com>
2022-12-13 13:52:47 +0530

    login

---
+++ Server/Controllers/FileController.cs
@@ -0,0 +1,20 @@
+﻿using CDPModule1.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CDPModule1.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        [System.Web.Http.Route("upload")]
+        [Authorize(Roles = "Admin")]
+        public ResponseModal UploadFile([FromBody] string fileBaseString)
+        {
+            return new ResponseModal();
+        }
+    }
+}
